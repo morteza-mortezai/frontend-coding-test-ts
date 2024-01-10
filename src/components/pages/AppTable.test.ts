55d878d8d073bca773e55ca48fb162a4a532b925
morteza <mrtz.mrtzi.1989@gmail.com>
2024-01-10 17:22:43 +0330

    add slot test

--- src/components/pages/AppTable.test.ts
+++ src/components/pages/AppTable.test.ts
@@ -1,17 +1,77 @@
 import { render } from '@testing-library/vue'
+import { mount, flushPromises } from "@vue/test-utils";
 import AppTable from './AppTable.vue'
 
-test('it should work', () => {
-  const { getByText } = render(AppTable, {
-    props: {
-      headers:['name','url'],
-      items:[
-        {name:'aa',url:'url'},
-        {name:'bb',url:'url bb'},
-    ]
-    }
+describe('component test', async () => {
+  const wrapper = mount(AppTable);
+  beforeEach(() => {
+    wrapper.setProps({
+      headers: ['name', 'url'],
+      items: [
+        { name: 'aa', url: 'url' },
+        { name: 'bb', url: 'url bb' },
+      ]
+    })
+    //  flushPromises()
   })
+  afterEach(() => {
+    wrapper.unmount()
+  })
+  test("AppTable Component renders the correct headers", () => {
+    const headers = wrapper.findAll('thead tr th')
+    expect(headers.at(0).text()).toBe('name')
+    expect(headers.at(1).text()).toBe('url')
+  });
 
-  // assert output
-  getByText('aa')
+  test("AppTable Component renders the correct rows", () => {
+    const rows = wrapper.findAll('tbody tr')
+    expect(rows.length).toEqual(2)
+  });
+
+  test("AppTable Component renders the correct items on a row", () => {
+    const items = wrapper.findAll('tbody tr td')
+    expect(items.at(0).text()).toBe('aa')
+    expect(items.at(1).text()).toBe('url')
+  });
+
 })
+
+describe('slot test', async () => {
+  const wrapper = mount(AppTable, {
+    props: {
+      headers: ['name', 'url'],
+      items: [
+        { name: 'aa', url: 'url' },
+        { name: 'bb', url: 'url bb' },
+      ]
+    },
+    slots: {
+      name: 'Header',
+    }
+  });
+  await flushPromises()
+
+  test("AppTable Component renders the correct slot", () => {
+
+    const items = wrapper.findAll('tbody tr td')
+    expect(items.at(0).text()).toBe('Header')
+  });
+
+})
+
+
+
+// test('it should work', () => {
+//   const { getByText } = render(AppTable, {
+//     props: {
+//       headers:['name','url'],
+//       items:[
+//         {name:'aa',url:'url'},
+//         {name:'bb',url:'url bb'},
+//     ]
+//     }
+//   })
+
+//   // assert output
+//   getByText('aa')
+// })
